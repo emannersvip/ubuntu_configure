f41927d0dc85378bcc178882d7a874468580797b
emannersvip <edson.manners@gmail.com>
2021-06-16 08:28:11 -0400

    Added htop

--- base_setup.pp
+++ base_setup.pp
@@ -1,4 +1,4 @@
 package {'git': ensure => installed}
 package {'puppet': ensure => installed}
 package {'vim': ensure => installed}
-#package {'xfce4-goodies': ensure => installed}
+package {'htop': ensure => installed}
