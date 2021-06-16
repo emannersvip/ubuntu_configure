1bcd9928a503bcb04de07d0145ab1040ff567074
emannersvip <edson.manners@gmail.com>
2021-06-16 07:59:43 -0400

    Added puppet

--- base_setup.pp
+++ base_setup.pp
@@ -1,4 +1,4 @@
 package {'git': ensure => installed}
-#package {'tightvncserver': ensure => installed}
+package {'puppet': ensure => installed}
 #package {'xfce4': ensure => installed}
 #package {'xfce4-goodies': ensure => installed}
