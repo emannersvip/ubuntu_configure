65db6623112454d07abc765d37f9ea71e778f517
emannersvip <edson.manners@gmail.com>
2021-06-18 08:12:35 -0400

    Added screen

--- base_setup.pp
+++ base_setup.pp
@@ -2,3 +2,4 @@ package {'git': ensure => installed}
 package {'puppet': ensure => installed}
 package {'vim': ensure => installed}
 package {'htop': ensure => installed}
+package {'screen': ensure => installed}
