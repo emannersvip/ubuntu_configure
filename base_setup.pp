a8341a2efd3a51654bd2d8c834dd2f4553a56a3c
emannersvip <edson.manners@gmail.com>
2021-07-05 07:34:32 -0400

    Updates

--- base_setup.pp
+++ base_setup.pp
@@ -1,5 +1,21 @@
-package {'git': ensure => installed}
-package {'puppet': ensure => installed}
-package {'vim': ensure => installed}
-package {'htop': ensure => installed}
-package {'screen': ensure => installed}
+# Build New Ubuntu host 2021
+#
+
+$homedir = '/home/emanners'
+
+file { "$homedir/Crypto":       ensure => directory, }
+file { "$homedir/Crypto/Chia":  ensure => directory, }
+
+#--
+package { 'htop':  ensure => installed, }
+package { 'iotop':  ensure => installed, }
+package { 'screen':  ensure => installed, }
+# https://forum.level1techs.com/t/how-to-reformat-520-byte-drives-to-512-bytes-usually/133021
+package { 'sg3-utils': ensure => installed, }
+package { 'smartmontools':  ensure => installed, }
+#--
+package { 'vim':  ensure => installed, }
+file { "$homedir/.vimrc":
+  ensure        => present,
+  content       => "colorscheme darkblue\nsyntax on",
+}
