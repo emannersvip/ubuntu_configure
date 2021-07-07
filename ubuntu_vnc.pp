5a3e71e858a713cf556cf225b824bcb6d6113c4c
Edson Manners <edson.manners@gmail.com>
2021-07-07 06:27:36 -0400

    Adding VNC and puppet file directory

---
+++ ubuntu_vnc.pp
@@ -0,0 +1,37 @@
+# https://linuxize.com/post/how-to-install-and-configure-vnc-on-ubuntu-20-04/
+
+package {'tigervnc-standalone-server': ensure => installed}
+package {'xfce4': ensure => installed}
+package {'xfce4-goodies': ensure => installed}
+
+file {'vnc_dir':
+  ensure => directory,
+  path   => '/home/emanners/.vnc',
+}
+
+file {'vnc_passwd':
+  ensure => present,
+  path   => '/home/emanners/.vnc/passwd',
+  mode   => '600',
+  source => '/home/emanners/Crypto/Chia/ChiaMove/puppet_files/passwd',
+}
+
+file {'vnc_xstartup':
+  ensure => present,
+  path   => '/home/emanners/.vnc/xstartup',
+  mode   => '700',
+  source => '/home/emanners/Crypto/Chia/ChiaMove/puppet_files/xstartup',
+}
+
+file {'vnc_config':
+  ensure => present,
+  path   => '/home/emanners/.vnc/config',
+  mode   => '700',
+  source => '/home/emanners/Crypto/Chia/ChiaMove/puppet_files/config',
+}
+
+exec {'vncserver':
+  path    => '/bin:/usr/bin',
+  command => 'vncserver -localhost no',
+  unless  => 'pgrep vncserver',
+}
